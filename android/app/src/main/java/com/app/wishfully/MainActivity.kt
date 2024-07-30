1214ef58a6f246ffb11c3cc32b55a40f3076c74a
suresh <suresh@jankosoft.com>
2024-07-30 20:06:20 +0530

    yml files

--- android/app/src/main/java/com/app/wishfully/MainActivity.kt
+++ android/app/src/main/java/com/app/wishfully/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.yohanpmdg.wishful
+package com.app.wishfully
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/app/wishfully/MainApplication.kt
+++ android/app/src/main/java/com/app/wishfully/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.yohanpmdg.wishful
+package com.app.wishfully
 
 import android.app.Application
 import android.content.res.Configuration
